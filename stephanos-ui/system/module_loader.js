dc71b15f4e8f8a5daad0bc4fc653d8ef145de9e8
Cheekyfellastef <stef.stef@hotmail.co.uk>
2026-03-12 16:48:03 +0000

    Update module_loader.js

--- stephanos-ui/system/module_loader.js
+++ stephanos-ui/system/module_loader.js
@@ -1,1 +1,31 @@
-Module loader
+class ModuleLoader {
+    constructor() {
+        this.modules = {};
+    }
+
+    async loadRegistry() {
+        const response = await fetch("./modules/module_registry.json");
+        const registry = await response.json();
+        return registry.modules;
+    }
+
+    async loadModules() {
+        const modules = await this.loadRegistry();
+
+        for (const module of modules) {
+            try {
+                const importedModule = await import(module.path);
+                this.modules[module.name] = importedModule;
+                console.log(`Module loaded: ${module.name}`);
+            } catch (error) {
+                console.error(`Failed to load module: ${module.name}`, error);
+            }
+        }
+    }
+
+    getModule(name) {
+        return this.modules[name];
+    }
+}
+
+export const moduleLoader = new ModuleLoader();
