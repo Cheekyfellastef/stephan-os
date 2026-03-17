a7b3fdfc6d914bcbba3d991af139f71eeb8b1941
Cheekyfellastef <stef.stef@hotmail.co.uk>
2026-03-17 16:03:46 +0000

    Configure build output directory for Vite

--- stephanos-ui/vite.config.js
+++ stephanos-ui/vite.config.js
@@ -3,4 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: './',
+  build: {
+    outDir: '../apps/stephanos/dist',
+    emptyOutDir: true
+  }
 });
