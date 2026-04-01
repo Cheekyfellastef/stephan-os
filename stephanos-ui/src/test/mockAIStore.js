161bf126a7bc866d3f5746a039fc0ae664efd18c
Cheekyfellastef <stef.stef@hotmail.co.uk>
2026-04-01 07:41:05 +0100

    test: wrap App render harness with AIStoreProvider

--- stephanos-ui/src/test/mockAIStore.js
+++ stephanos-ui/src/test/mockAIStore.js
@@ -2,6 +2,10 @@ export function __setMockStore(nextStore) {
   globalThis.__STEPHANOS_TEST_AI_STORE__ = nextStore && typeof nextStore === 'object' ? nextStore : {};
 }
 
+export function AIStoreProvider({ children }) {
+  return children;
+}
+
 export function useAIStore() {
   return globalThis.__STEPHANOS_TEST_AI_STORE__ || {};
 }
