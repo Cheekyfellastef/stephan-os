b93f45bb2ebabad98ee346956ae00fec99f78dfe
Cheekyfellastef <stef.stef@hotmail.co.uk>
2026-07-14 20:03:30 +0100

    Add Quest entry offline cache

---
+++ apps/spatial-bridge/service-worker.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = 'stephanos-spatial-bridge-v0';
+const CORE_ASSETS = [
+  './',
+  './quest-entry.html',
+  './index.html',
+  './bridge-state.v0.json',
+  './manifest.webmanifest',
+  './offline.html',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(CORE_ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+
+  const requestUrl = new URL(event.request.url);
+  if (requestUrl.origin !== self.location.origin) return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        if (response && response.ok) {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        }
+        return response;
+      })
+      .catch(async () => {
+        const cached = await caches.match(event.request);
+        return cached || caches.match('./offline.html');
+      })
+  );
+});
