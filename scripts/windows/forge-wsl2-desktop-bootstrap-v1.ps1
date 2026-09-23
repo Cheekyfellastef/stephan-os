d8b336f14396e87c1bb0518a420d54c7fe90ac85
Cheekyfellastef <stef.stef@hotmail.co.uk>
2026-09-23 23:28:30 +0100

    Emit only terminal Forge desktop receipts

--- scripts/windows/forge-wsl2-desktop-bootstrap-v1.ps1
+++ scripts/windows/forge-wsl2-desktop-bootstrap-v1.ps1
@@ -189,12 +189,6 @@ try {
     $launcherStream.Write($launcherBytes, 0, $launcherBytes.Length)
     $launcherStream.Flush($true)
     $launcherStream.Position = 0
-    Emit-Receipt $false 'BLOCKED' 'FORGE_WSL2_OPERATOR_DESKTOP_LAUNCH_REQUIRED' @{
-        desktopLauncherName = $LauncherName
-        mutationPerformed = $false
-        launcherLocked = $true
-    }
-
     $deadline = [DateTime]::UtcNow.AddSeconds($LauncherWaitSeconds)
     while ([DateTime]::UtcNow -lt $deadline -and -not (Test-ElevatedReceiptReady)) {
         Start-Sleep -Milliseconds 500
